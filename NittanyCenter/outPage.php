8792af1e5e61348c58ce10ff8f52ec248732937e
Alfred Shaker <ashaker@kent.edu>
2013-03-23 11:29:46 -0400

    file update

--- NittanyCenter/outPage.php
+++ NittanyCenter/outPage.php
@@ -1,11 +1,16 @@
 <?php
 // get variables from your form
 $name=$_POST['name'];
-
+$name=$_POST['category'];
+$name=$_POST['isbn'];
+$name=$_POST['desc'];
 // then you can display them however you want.
 ?>
 <html>
 <body>
-Welcome <?php echo $_POST["name"]; ?>
+Welcome <?php echo $_POST["name"]; ?><br>
+<?php echo $_POST["category"]; ?><br>
+<?php echo $_POST["isbn"]; ?><br>
+<?php echo $_POST["desc"]; ?><br>
 </body>
 </html>
